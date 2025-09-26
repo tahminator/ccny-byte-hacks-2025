2a85f2a4187a81a81272099e540e48430daedd3f
Angela Yu <angelayu0530@gmail.com>
2025-09-26 14:06:26 -0400

    changes so you can use api client

--- api/api.go
+++ api/api.go
@@ -1,14 +1,16 @@
 package api
 
 import (
+
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/tahminator/go-react-template/api/auth"
 	"github.com/tahminator/go-react-template/database/repository/session"
 	"github.com/tahminator/go-react-template/database/repository/user"
+	"google.golang.org/genai"
 )
 
-func NewRouter(eng *gin.Engine, db *pgxpool.Pool) *gin.RouterGroup {
+func NewRouter(eng *gin.Engine, db *pgxpool.Pool, geminiClient *genai.Client) *gin.RouterGroup {
 	r := eng.Group("/api")
 
 	userRepository := user.NewPostgresUserRepository(db)
