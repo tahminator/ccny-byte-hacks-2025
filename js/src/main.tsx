f945e2a2aaeb4287e0d7543a4746e24eb49f010f
Angela Yu <angelayu0530@gmail.com>
2025-09-27 06:35:52 -0400

    changes to main.tsx based off vite

--- js/src/main.tsx
+++ js/src/main.tsx
@@ -8,14 +8,17 @@ import { Toaster } from "sonner";
 
 import ReactQueryProvider from "@/lib/query";
 import { router } from "@/lib/router";
+import { ThemeProvider } from "@/lib/themeProvider";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ReactQueryProvider>
-      <Toaster />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ReactQueryProvider>
+        <Toaster />
       <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </ReactQueryProvider>
+        <ReactQueryDevtools />
+      </ReactQueryProvider>
+    </ThemeProvider>
   </StrictMode>
 );
