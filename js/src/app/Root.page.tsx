779a2ce669ed75a443d6437aa62ae692a42f0c6d
Angela Yu <angelayu0530@gmail.com>
2025-09-27 02:15:29 -0400

    changed root to only pop up when isConflicted

--- js/src/app/Root.page.tsx
+++ js/src/app/Root.page.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import type { CodeDirectory, CodeFile } from "@/lib/api/types/code";
 
 import CodeEditor from "@/components/code/CodeEditor";
@@ -9,20 +11,28 @@ import testFiles from "./test.json";
 const files: (CodeFile | CodeDirectory)[] = testFiles as unknown as any;
 
 export default function RootPage() {
+  const [selectedFile, setSelectedFile] = useState<CodeFile | null>(null);
+
   const handleResolveConflict = (context: string) => {
     console.log("Resolving merge conflict with context:", context);
+    console.log("Selected file:", selectedFile);
+  };
+
+  const handleFileSelected = (file: CodeFile) => {
+    console.log("File selected:", JSON.stringify(file));
+    setSelectedFile(file);
   };
 
   return (
     <div className="relative flex w-[100vw] h-[100vh] justify-center items-start pt-16">
       <MergeConflictButton
-        selectedFile={null}
+        selectedFile={selectedFile}
         onResolveConflict={handleResolveConflict}
       />
       <CodeEditor
         files={files}
         title={"My Project"}
-        onFileSelected={(file) => console.log(JSON.stringify(file))}
+        onFileSelected={handleFileSelected}
       />
       <div className="flex min-w-1/6"></div>
     </div>
